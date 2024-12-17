2a8281562b67c6b80c7020fd0d3f77e2e450c949
leonardotimo <coderhouseleo@gmail.com>
2024-12-17 18:45:54 -0300

    se modifico descripcion y se agrego enlace thumbnails

--- src/routers/cartRouter.js
+++ src/routers/cartRouter.js
@@ -1,81 +1,85 @@
-const fs = require('fs/promises');
-const path = require('path');
-const express = require('express');
-const {v4: uuidv4} = require('uuid');
+const fs = require("fs/promises");
+const path = require("path");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 // Configuracion del router de carrito
-const cartRouter = express.Router()
+const cartRouter = express.Router();
 
 // Sirve para probar que la ruta es correcta.
-const filePath = path.join(__dirname, '../db/cart.json');
-const productsFilePath =  path.join(__dirname, '../db/products.json');
+const filePath = path.join(__dirname, "../db/cart.json");
+const productsFilePath = path.join(__dirname, "../db/products.json");
 
 // Las rutas de carrito
-cartRouter.post('/', async (request, response) => {
-    const cart = request.body
+cartRouter.post("/", async (request, response) => {
+  const cart = request.body;
 
-    const data = await fs.readFile(filePath, 'utf-8');
+  const data = await fs.readFile(filePath, "utf-8");
 
-    const carts = JSON.parse(data)
+  const carts = JSON.parse(data);
 
-    carts.push({id: uuidv4(), ...cart})
+  carts.push({ id: uuidv4(), ...cart });
 
-    await fs.writeFile(filePath, JSON.stringify(carts), 'utf-8')
+  await fs.writeFile(filePath, JSON.stringify(carts), "utf-8");
 
-    response.json({message: "Nuevo carrito generado"})
-})
+  response.json({ message: "Nuevo carrito generado" });
+});
 
-cartRouter.get('/', async (request, response) => {
-    const data = await fs.readFile(filePath, 'utf-8');
+cartRouter.get("/", async (request, response) => {
+  const data = await fs.readFile(filePath, "utf-8");
 
-    const carts = JSON.parse(data)
+  const carts = JSON.parse(data);
 
-    response.json(carts)
-}) 
+  response.json(carts);
+});
 
-cartRouter.get('/:cid', async (request, response) => {
-    const {cid} = request.params;
+cartRouter.get("/:cid", async (request, response) => {
+  const { cid } = request.params;
 
-    const data = await fs.readFile(filePath, 'utf-8');
+  const data = await fs.readFile(filePath, "utf-8");
 
-    const carts = JSON.parse(data)
+  const carts = JSON.parse(data);
 
+  const cartById = carts.find((cart) => cart.id == cid);
 
-    const cartById = carts.find(cart => cart.id == cid)
+  response.json(cartById.products);
+});
 
-    response.json(cartById.products)
-})
+cartRouter.post("/:cid/product/:pid", async (request, response) => {
+  const { cid, pid } = request.params;
 
-cartRouter.post('/:cid/product/:pid', async (request,response) => {
-   const {cid, pid} = request.params;
+  const cartData = await fs.readFile(filePath, "utf-8");
+  const productsData = await fs.readFile(productsFilePath, "utf-8");
 
-   const cartData = await fs.readFile(filePath, 'utf-8');
-   const productsData = await fs.readFile(productsFilePath, 'utf-8');
+  const carts = JSON.parse(cartData);
+  const products = JSON.parse(productsData);
 
-   const carts = JSON.parse(cartData)
-   const products = JSON.parse(productsData)
+  const product = products.find((product) => product.id == pid);
+  let cartById = carts.find((cart) => cart.id == cid);
 
-   const product = products.find(product => product.id == pid)
-   let cartById = carts.find(cart => cart.id == cid)
-   console.log(cartById)
-   
-   const itemInCart = cartById.products.find(item => item.product == product.id)
+  const itemInCart = cartById.products.find(
+    (item) => item.product == product.id
+  );
 
-   if(!itemInCart){
-    cartById.products.push({product: product.id, quantity: 1})
-   } else {
-    let prevQuantity = itemInCart.quantity
-    cartById = {...cartById, products: cartById.products.filter(item => item.product != itemInCart.product)}
-    
-    cartById.products.push({product: product.id, quantity: prevQuantity + 1})
-   }
-   
-   const newCarts = carts.filter(cart => cart.id != cartById.id)
-   newCarts.push(cartById)
-   await fs.writeFile(filePath, JSON.stringify(newCarts), 'utf-8')
-   
-   response.json({message: "Producto agregado al carrito"})
-})
+  if (!itemInCart) {
+    cartById.products.push({ product: product.id, quantity: 1 });
+  } else {
+    let prevQuantity = itemInCart.quantity;
+    cartById = {
+      ...cartById,
+      products: cartById.products.filter(
+        (item) => item.product != itemInCart.product
+      ),
+    };
 
+    cartById.products.push({ product: product.id, quantity: prevQuantity + 1 });
+  }
+
+  const newCarts = carts.filter((cart) => cart.id != cartById.id);
+  newCarts.push(cartById);
+  await fs.writeFile(filePath, JSON.stringify(newCarts), "utf-8");
+
+  response.json({ message: "Producto agregado al carrito" });
+});
 
 module.exports = cartRouter;
