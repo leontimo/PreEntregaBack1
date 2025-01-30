22c508ff84c8d1bd59f5873b3c16668cf6b96cc5
leonardotimo <coderhouseleo@gmail.com>
2025-01-30 11:27:21 -0300

    se agrego el git ignore

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 import server from "./server/server.js";
 import { Server } from "socket.io";
-import products from "./db/products.json" assert {type: 'json'};
+import products from "./db/products.json" assert { type: "json" };
 
 const PORT = 8080;
 
-
-// Configuracion del server -> Se delega -> server.js
-
+// Configuracion del server -> Se delega -> server.jsnp
 
 // WebSocket config
-const app = server.listen(PORT, () => console.log(`Server funcionando en el puerto ${PORT}`))
+const app = server.listen(PORT, () =>
+  console.log(`Server funcionando en el puerto ${PORT}`)
+);
 
 export const io = new Server(app);
 
 io.on("connection", (socket) => {
-    console.log("New client connected:", socket.id);
+  console.log("New client connected:", socket.id);
 
-    socket.emit("init", products);
+  socket.emit("init", products);
 });
