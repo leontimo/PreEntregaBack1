45ca1f0db748771682c48b1ef56720551ce9a676
leonardotimo <coderhouseleo@gmail.com>
2025-02-07 20:59:55 -0300

    objeto de paginacion

--- src/routers/view.routes.js
+++ src/routers/view.routes.js
@@ -1,45 +1,51 @@
 import { Router } from "express";
-import products from "../db/products.json" assert { type: "json" };
-import Product from "../models/product.model.js";
+import Product from "../models/product.model.js"
+import Cart from "../models/cart.model.js"
 
-export const viewsRoutes = Router();
+ export const viewsRoutes = Router();
 
-viewsRoutes.get("/products", async (request, response) => {
-  let { page = 1, limit = 10 } = request.query;
-  try {
-    const products = await Product.paginate(
-      {},
-      { page: Number(page), limit: Number(limit) }
-    );
+ // Muestra todos los productos con su respectiva paginacion
+ viewsRoutes.get("/products", async (request, response) => {
+   let { page = 1, limit = 10 } = request.query;
+   try {
 
-    if (products.docs.length === 0) {
+     const products = await Product.paginate(
+       {},
+       { page: Number(page), limit: Number(limit)}
+     );
+
+     console.log(products)
+
+     if(products.docs.length === 0){
       products.isEmpty = true;
-    }
+     }
 
-    if (products.docs.length > limit) {
-      products.hasNextPage = true;
-    }
+     if(products.docs.length > limit){
+       products.hasNextPage = true;
+     }
 
-    products.nextPage = () => {
-      page += 1;
-      products.page += 1;
-      return products.page;
-    };
-    products.concurrentPage = () => {
-      if (products.hasNextPage === false) {
-        return products.page;
-      }
-      return products.page - 1;
-    };
+     products.nextPage = () => {
+        return products.page + 1
+     }
 
-    response.render("realTimeProducts", { products });
+     response.render("realTimeProducts", {products})
 
-    console.log(products.docs.length);
-  } catch (error) {
-    response.status(500).json({ message: error.message });
+   } catch (error) {
+     response.status(500).json({ message: error.message });
+   }
+ });
+
+// Muestra un carrito por ID
+viewsRoutes.get('/carts/:cid', async (request, response) => {
+  const { cid } = request.params;
+
+  const cartById = await Cart.findById({ _id: cid });
+
+  const products = cartById.products
+
+  if (!cartById) {
+    return { message: "Carrito no encontrado", statusCode: 404 };
   }
-});
-
-viewsRoutes.get("/", (req, res) => {
-  res.render("home", products);
-});
+  
+  response.render("realTimeCart", { products });
+})
